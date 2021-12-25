30318f7593912a545533210ac54edfce72c750ad
Ralph Romero <ralph@romero.id.au>
2021-12-25 16:04:12 +1100

    day2

---
+++ CSharpDay2/Program.cs
@@ -0,0 +1,16 @@
+﻿var items = CSharpDay2.Resources.Input.Split('\n');
+int horizontal = 0;
+int vertical = 0;
+foreach (var item in items)
+{
+    if (item.StartsWith("forward"))
+        horizontal += int.Parse(item.Split(' ')[1]);
+    if (item.StartsWith("down"))
+        vertical += int.Parse(item.Split(' ')[1]);
+    if (item.StartsWith("up"))
+        vertical -= int.Parse(item.Split(' ')[1]);
+    //Console.WriteLine($"Horizontal: {horizontal}, Vertical: {vertical}, Multiplied: {horizontal * vertical}");
+    //Console.ReadLine();
+}
+
+Console.WriteLine($"Horizontal: {horizontal}, Vertical: {vertical}, Multiplied: {horizontal * vertical}");

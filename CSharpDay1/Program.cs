762f9a893da461e78ae86598d6936082d56cb99d
Ralph Romero <ralph@romero.id.au>
2021-12-25 15:29:25 +1100

    Add project files.

---
+++ CSharpDay1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine(CSharpDay1.Properties.Resources.Input);
+var items = CSharpDay1.Properties.Resources.Input.Split('\n');
+int count = 0;
+for(int i = 1; i < items.Length; i++)
+{
+    try
+    {
+        if (int.Parse(items[i]) > int.Parse(items[i - 1]))
+            count++;
+    }
+    catch (Exception) { }
+}
+Console.WriteLine("Total: " + count);

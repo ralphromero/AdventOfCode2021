86f75389a5698d0a83a4e8c24f0cc34fc766ac59
Ralph Romero <ralph@romero.id.au>
2021-12-27 09:34:03 +1100

    day 1 complete

---
+++ CSharpDay1/Parts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDay1
+{
+    internal class Parts
+    {
+        public static void Part1(string[] items)
+        {
+            int count = 0;
+            for (int i = 1; i < items.Length; i++)
+            {
+                try
+                {
+                    if (int.Parse(items[i]) > int.Parse(items[i - 1]))
+                        count++;
+                }
+                catch (Exception) { }
+            }
+            Console.WriteLine("Total: " + count);
+        }
+
+        public static void Part2(string[] items)
+        {
+            var output = new string[items.Length];
+            for (int i = 0; i < items.Length; i++)
+            {
+                var first = int.Parse(items[i]);
+                var second = (i + 1) < items.Length ? int.Parse(items[i+1]) : 0;
+                var third = (i + 2) < items.Length ? int.Parse(items[i + 2]) : 0;
+                
+                output[i] = Convert.ToString(first + second + third);
+            }
+            Part1(output);
+        }
+    }
+}
--- CSharpDay1/Program.cs
+++ CSharpDay1/Program.cs
@@ -1,14 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine(CSharpDay1.Properties.Resources.Input);
+﻿using CSharpDay1;
 var items = CSharpDay1.Properties.Resources.Input.Split('\n');
-int count = 0;
-for(int i = 1; i < items.Length; i++)
-{
-    try
-    {
-        if (int.Parse(items[i]) > int.Parse(items[i - 1]))
-            count++;
-    }
-    catch (Exception) { }
-}
-Console.WriteLine("Total: " + count);
+Console.WriteLine("Advent of Code day 1");
+Console.WriteLine("Part1");
+Parts.Part1(items);
+Console.WriteLine("Part2");
+Parts.Part2(items);
